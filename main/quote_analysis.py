5b538195ab2fbe55f9bc0a8aa6b850c8f4f97169
Saadat <saadatssu@gmail.com>
2022-03-02 16:00:51 +0600

    Add get quotes logic

---
+++ main/quote_analysis.py
@@ -0,0 +1,26 @@
+def count_total_letters(quote):
+    counter = 0
+    for i in quote:
+        if i.isalpha():
+            counter += 1
+    return counter
+
+
+def count_vowels(quote):
+    return -1
+
+
+def count_consonants(quote):
+    return -1
+
+
+def count_repetitions(quote):
+    return -1
+
+
+def count_average_word_length(quote):
+    pass
+
+
+def get_longest_words(quote):
+    pass
--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+import requests
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -6,4 +7,15 @@ from rest_framework.views import APIView
 class GetQuotesView(APIView):
 
     def get(self, request):
-        return Response({}, status=status.HTTP_200_OK)
+        quotes_api_url = 'https://api.kanye.rest/'
+        counter = 10
+        quotes = []
+
+        while counter:
+            response = requests.get(url=quotes_api_url).json()
+            quote = response.get('quote')
+
+            quotes.append(quote)
+            counter -= 1
+
+        return Response({"quotes": quotes}, status=status.HTTP_200_OK)
