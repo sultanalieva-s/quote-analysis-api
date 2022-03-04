58392c338106ce1a951fad5b0d0def1059ab12a2
Saadat <saadatssu@gmail.com>
2022-03-04 13:24:41 +0600

    Fix response format

--- main/serializers.py
+++ main/serializers.py
@@ -6,16 +6,18 @@ from main.models import Quote, QuoteAnalysis
 class QuoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Quote
-        fields = ("quote",)
+        fields = ("id",
+                  "quote",)
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         representation['analysis'] = QuoteAnalysisSerializer(QuoteAnalysis.objects.get(quote=instance)).data
-
         return representation
 
 
 class QuoteAnalysisSerializer(serializers.ModelSerializer):
+    quote = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Quote.objects.all())
+
     class Meta:
         model = QuoteAnalysis
         fields = (
